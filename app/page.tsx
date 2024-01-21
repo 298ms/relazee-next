c7b1f76fc26966320932553fda20ebbfe5cc61a6
Nafis Handoko <naffisioner@gmail.com>
2024-01-22 01:16:18 +0700

    style: wording in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <div className='w-full h-screen container mx-auto flex flex-col items-center justify-center gap-3 text-center'>
             <h1 className='text-relazee-text-heading font-bold text-2xl'>Relazee is still in development, stay tuned!</h1>
-            <Link href={`/feed`} className='bg-relazee-orange text-white rounded-lg font-medium text-sm whitespace-nowrap px-4 py-3'>Go to Homepage</Link>
+            <Link href={`/feed`} className='bg-relazee-orange text-white rounded-lg font-medium text-sm whitespace-nowrap px-4 py-3'>Take a peek at the homepage</Link>
         </div>
     )
 }
